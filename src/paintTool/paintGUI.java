0faf8ee6194d24376fd434080a3bb3cac786f192
sxsg430 <872597+sxsg430@users.noreply.github.com>
2019-05-09 23:41:05 +1000

    Add enough code to draw a basic window with no contents.

--- src/paintTool/paintGUI.java
+++ src/paintTool/paintGUI.java
@@ -1,11 +1,13 @@
 package paintTool;
 
 import javax.swing.*;
+import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
 public class paintGUI extends JFrame implements ActionListener, Runnable {
-
+    private static final int WIDTH = 600;
+    private static final int HEIGHT = 500;
     @Override
     public void actionPerformed(ActionEvent e) {
 
@@ -13,10 +15,18 @@ public class paintGUI extends JFrame implements ActionListener, Runnable {
 
     @Override
     public void run() {
-
+        createGUI();
+    }
+    private void createGUI() {
+        setSize(WIDTH,HEIGHT);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setLayout(new BorderLayout());
+        repaint();
+        setVisible(true);
     }
 
     public static void main(String[] args) {
-
+        paintGUI GUI = new paintGUI();
+        GUI.run();
     }
 }
