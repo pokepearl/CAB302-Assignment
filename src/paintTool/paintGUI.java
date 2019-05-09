e80b68441368773a6c0d60016abc4e4ded93bd54
sxsg430 <872597+sxsg430@users.noreply.github.com>
2019-05-09 23:23:03 +1000

    Setup Swing imports and create main function

--- src/paintTool/paintGUI.java
+++ src/paintTool/paintGUI.java
@@ -1,4 +1,22 @@
 package paintTool;
 
-public class paintGUI {
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class paintGUI extends JFrame implements ActionListener, Runnable {
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+
+    }
+
+    @Override
+    public void run() {
+
+    }
+
+    public static void main(String[] args) {
+
+    }
 }
