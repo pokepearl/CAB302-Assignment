945ca4375438b210e0513a02c3830f857b0800a8
sxsg430 <872597+sxsg430@users.noreply.github.com>
2019-05-10 20:53:50 +1000

    Add Panels for elements in UI (somewhat placeholders)

--- src/paintTool/paintGUI.java
+++ src/paintTool/paintGUI.java
@@ -8,6 +8,10 @@ import java.awt.event.ActionListener;
 public class paintGUI extends JFrame implements ActionListener, Runnable {
     private static final int WIDTH = 600;
     private static final int HEIGHT = 500;
+    private JPanel pnlNavbar;
+    private JPanel pnlSidebar;
+    private JPanel pnlBottombar;
+    private JPanel pnlEditArea;
     @Override
     public void actionPerformed(ActionEvent e) {
 
@@ -21,8 +25,21 @@ public class paintGUI extends JFrame implements ActionListener, Runnable {
         setSize(WIDTH,HEIGHT);
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         setLayout(new BorderLayout());
+        pnlNavbar = createPanel(Color.GRAY);
+        pnlBottombar = createPanel(Color.GRAY);
+        pnlSidebar = createPanel(Color.LIGHT_GRAY);
+        pnlEditArea = createPanel(Color.WHITE);
+        getContentPane().add(pnlNavbar,BorderLayout.NORTH);
+        getContentPane().add(pnlBottombar,BorderLayout.SOUTH);
+        getContentPane().add(pnlSidebar,BorderLayout.WEST);
+        getContentPane().add(pnlEditArea,BorderLayout.CENTER);
         repaint();
         setVisible(true);
+    }
+    private JPanel createPanel(Color c) {
+        JPanel temp = new JPanel();
+        temp.setBackground(c);
+        return temp;
     }
 
     public static void main(String[] args) {
