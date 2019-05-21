26ea82fcb4065742fbac7e6c17e3804b5f34bc5b
sxsg430 <872597+sxsg430@users.noreply.github.com>
2019-05-21 22:19:46 +1000

    Add some more unit tests.

--- src/paintTool/paintToolJUnit.java
+++ src/paintTool/paintToolJUnit.java
@@ -6,15 +6,47 @@ import paintTool.ShapePoint;
 public class paintToolJUnit {
 
     @Test
-    public void testPointSize() {
+    public void testPointContent() {
         ShapePoint test = new ShapePoint();
         test.addToArray(102, 225);
         assertEquals("{102.0=225.0}", test.printArray());
     }
     @Test
-    public void testPointSizeFalse() {
+    public void testPointContentFalse() {
         ShapePoint test = new ShapePoint();
         test.addToArray(102, 225);
         assertNotEquals("{102.0=227.0}", test.printArray());
+    }
+    @Test
+    public void testPointContentMulti() {
+        ShapePoint test = new ShapePoint();
+        test.addToArray(102, 225);
+        test.addToArray(95, 102);
+        System.out.println(test.printArray());
+        assertEquals("{102.0=225.0, 95.0=102.0}", test.printArray());
+    }
+    @Test
+    public void testPointContentMultiFalse() {
+        ShapePoint test = new ShapePoint();
+        test.addToArray(102, 225);
+        test.addToArray(95, 102);
+        assertNotEquals("{102.0=225.0}, {95.0=105.0}", test.printArray());
+    }
+
+    @Test
+    public void testPointSize() {
+        ShapePoint test = new ShapePoint();
+        test.addToArray(102, 225);
+        test.addToArray(1, 20);
+        test.addToArray(132, 233);
+        assertEquals(3, test.sizeOfArray());
+    }
+    @Test
+    public void testPointSizeFalse() {
+        ShapePoint test = new ShapePoint();
+        test.addToArray(102, 225);
+        test.addToArray(1, 20);
+        test.addToArray(132, 233);
+        assertNotEquals(5, test.sizeOfArray());
     }
 }
